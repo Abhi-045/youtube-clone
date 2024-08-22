9107b350dcfd06584034ff1d907d8471a834beee
Abhi Gunjal <151115540+Abhi-045@users.noreply.github.com>
2024-08-22 23:16:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var menuIcon = document.querySelector(".menu-icon")
+var sidebar = document.querySelector(".sidebar")
+var mainContainer = document.querySelector(".mainContainer")
+var card = document.querySelector(".card")
+console.log("menuIcon", menuIcon);
+ 
+menuIcon.onclick =function(){
+    sidebar.classList.toggle("smaller-sidebar")
+    mainContainer.classList.toggle("larger-mainContainer")
+    card.classList.toggle("larger-card")
+    console.log("menu icon clicked");
+
+}
